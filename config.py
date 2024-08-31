b165ef97c94aaed63bef8be017a789e735f75617
Warxhunter <god56yt@gmail.com>
2024-08-31 20:14:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7260899411:AAGbag8Ex_mdUE4KNBY4KyvIDYQGCXtN2n4")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("WarXhunter")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1665842628")
 
 
 
